6a34ef3a647a7ca9b12c33c34da79fbffd53fffd
Chris Wilcox <christopher.r.wilcox@gmail.com>
2018-05-24 11:30:35 +1000

    added a little basic error handling to the import resource and cleaned up the swagger/openapi doc a little

--- python/rest/import_resource.py
+++ python/rest/import_resource.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from threading import Thread
 from business.csv_processor import process_csv
 from .decorators import run_in_thread
+from werkzeug.exceptions import BadRequest
 
 import_resource = Blueprint("import_resource", __name__)
 
@@ -9,7 +10,10 @@ import_resource = Blueprint("import_resource", __name__)
 @import_resource.route("/import/", methods=["POST"])
 def import_crimes():
     if request.headers["Content-Type"] == "application/json":
-        _importFromCsv(request.get_json()["filename"])
+        filename = request.get_json()["filename"]
+        if filename is None:
+            raise BadRequest("filename must be provided")
+        _importFromCsv(filename)
         return "Import started", 201
 
 
